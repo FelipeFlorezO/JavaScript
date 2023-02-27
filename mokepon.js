1817d0103e9fdb7ee6821180de6f7d47d451745c
Felipe Florez Olivera <114842657+FelipeFlorezO@users.noreply.github.com>
2023-02-27 11:53:09 -0500

    Add files via upload

---
+++ mokepon.js
@@ -0,0 +1,8 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+function seleccionarMascotaJugador(){
+    alert("SELECCIONASTE TU MASCOTA")
+}
+window.addEventListener('load', iniciarJuego)
